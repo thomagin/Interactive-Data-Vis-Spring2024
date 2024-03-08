2791579b10bf14acdb9e2bed739a1bf3a428d6b1
thomagin <tom.fagin@gmail.com>
2024-03-07 20:08:17 -0500

    modified:   data/HOOD.csv

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,12 +1,21 @@
  /* CONSTANTS AND GLOBALS */
  const width = window.innerWidth * 0.7,
  height = window.innerHeight * 0.7,
- margin = { top: 20, bottom: 50, left: 60, right: 60 };
+ margin = { top: 20, bottom: 50, left: 60, right: 60 }
+ const parseDate = d3.timeParse("%m/%d/%y");
+ const formatDate = d3.timeFormat("%m/%d/%Y")
+;
 
 /* LOAD DATA */
-d3.csv('/Users/tfagin/Desktop/repositories/Interactive-Data-Vis-Spring2024/data/HOOD.csv', d3.autoType)
+d3.csv('../data/HOOD.csv', d3.autoType)
  .then(data => {
   console.log('data :>> ', data);
+
+  data.forEach(d => {
+    d.date = parseDate(d.date);
+    console.log('Parsed Date:', d.date);
+
+  
  
  // return {
   //   date: new Date(+d.date, 0, 1),
@@ -16,29 +25,29 @@ d3.csv('/Users/tfagin/Desktop/repositories/Interactive-Data-Vis-Spring2024/data/
 
   // SCALES
     const xScale = d3.scaleTime()
-      .domain(d3.extent[data, d => d.date])
-      .range([margin.left, width - margin.right])
+      .domain(d3.extent(data, d => d.date))
+      .range([margin.left, width - margin.right]);
 
     const yScale = d3.scaleLinear()
-      .domain(0, d3.max(data, d = d.close_last))
-      .range[(height - margin.bottom, margin.top)]
+      .domain([0, d3.max(data, d => d.close_last)])
+      .range([height - margin.bottom, margin.top]);
 
   // CREATE SVG ELEMENT
   const svg = d3.select("#container")
     .append("svg")
     .attr("width", width)
-    .attr("height", height)
+    .attr("height", height);
 
   // BUILD AND CALL AXES
 
   const xAxis = d3.axisBottom(xScale);
   svg.append("g")
-    .attr("transform", 'translate(0, ${height - margin.bottom})')
+    .attr("transform", `translate(0,${height - margin.bottom})`)
     .call(xAxis);
 
   const yAxis = d3.axisLeft(yScale);
   svg.append("g")
-    .attr("transform", 'translate(0, ${margin.left}, 0)')
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
     .call(yAxis);
 
   // LINE GENERATOR FUNCTION
@@ -51,37 +60,38 @@ d3.csv('/Users/tfagin/Desktop/repositories/Interactive-Data-Vis-Spring2024/data/
 
 
   svg.append("path")
-    .data(data)
+    .datum(data)
     .attr("fill", "pink")
     .attr("opacity", 0.7)
     .attr("d", areaGen);
 
+  });
+
   //  svg.selectAll(".circle")
-   /*    .data(date)
-      .join("circle")
-      .attr("cx", d => xScale(d.date))
-      .attr("cy", d => yScale(d.close_last))
-      .attr("r", 1) */
+   //    .data(date)
+   //   .join("circle")
+   //   .attr("cx", d => xScale(d.date))
+   //   .attr("cy", d => yScale(d.close_last))
+   //   .attr("r", 1) 
 
   //  svg.selectall(".afghanistan")
-/*       .data(data)
-      .join("path")
-      .attr("class", 'afghanistan')
-      .attr("fill", "none")
-      .attr("stroke", "black")
-      .attr("d", d => lineGen(d)) */
-
+  //     .data(data)
+  //    .join("path")
+  //    .attr("class", 'afghanistan')
+  //    .attr("fill", "none")
+  //    .attr("stroke", "black")
+  //    .attr("d", d => lineGen(d)) 
 
       //generator function
     //  const groupedData = d3.groups(data, d => d.country)
 
-/*       svg.selectAll("path.country")
-        .data(groupedData)
-        .join("path")
-        .attr("class", "date")
-        // .attr("d", d => lineGen(d[1])) this looks at the second item in the array for each country
-        .attr("d", (close_last, data]) => lineGen(data))
-        .attr("fill", "none")
-        .attr("stroke", "black") */
+//       svg.selectAll("path.country")
+//        .data(groupedData)
+//        .join("path")
+//        .attr("class", "date")
+//        // .attr("d", d => lineGen(d[1])) this looks at the second item in the array for each country
+//        .attr("d", (close_last, data]) => lineGen(data))
+//        .attr("fill", "none")
+//        .attr("stroke", "black")
 
-});
+ })
