6727852bdab1a30657bf4210d2e2ce7c4b6c6a07
thomagin <tom.fagin@gmail.com>
2024-03-11 22:46:46 -0400

    modified:   2_4_geographic/main.js

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+ const width = window.innerWidth * 0.9,
+   height = window.innerHeight * 0.7,
+   margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 /**
  * LOAD DATA
@@ -11,8 +11,8 @@
   d3.json("../data/world.json"),
   d3.csv("../data/MoMA_nationalities.csv", d3.autoType),
 ]).then(([geojson, nationalities]) => {
-  console.log(geojscon)
-  console.log(capitals)
+  console.log(geojson)
+  console.log(nationalities)
 
 
   const svg = d3 
@@ -23,38 +23,47 @@
   
   // SPECIFY PROJECTION
  
-    const projection = d3.geoAlbersUsa() //look up global projection!
-      .fitSize(
-        [width, height],
-        geojson
-      );
+  const projection = d3.geoMercator() //look up global projection!
+    .fitSize([width, height], geojson);
 
-    
   // DEFINE PATH FUNCTION
 
-  const geoPathGen = d3.geoPath(projection)
+  const geoPathGen = d3.geoPath(projection);
 
   // APPEND GEOJSON PATH  
 
-  svg.selectAll("us.state")
-        .data(geojson.features) //not just geoJson, but each one of the individual features, not an array per se
-        .join("path")
-        .attr("class", "us-state")
-        .attr("stroke", "black")
-        .attr("fill", "transparent")
-        .attr("d", d => geoPathGen(d)) //path mostly good if you're using the lines around the states
+  svg.selectAll(".property")
+  .data(geojson.features)
+  .join("path")
+  .attr("class", "country")
+  .attr("stroke", "black")
+  .attr("fill", "transparent")
+  .attr("d", d => geoPathGen(d));
 
   // APPEND DATA AS SHAPE
-        svg.selectAll(".us-capital")
-        .data(capitals)
-        .join("circle")
-        .attr("class", "us-capital") //make sure the class passed in matches initial selector in selectAll
-        .attr("r", 3)
-        //.attr("cx", 20)
-        //.attr("cy", 20)
-        .attr("transform", d => {
-          const point = projection([d.longitude, d.latitude])
-         return `translate(${point[0]},${point[1]})`
-          })
+  svg.selectAll(".nationality")
+    .data(nationalities)
+    .join("circle")
+    .attr("class", "nationality")
+    .attr("r", d => Math.sqrt(d.Count) * 0.3) // sizing the dots
+    .attr("fill", "pink")
+    .attr("cx", d => {
+      const country = d.Country; 
+      const feature = geojson.features.find(f => f.properties.name === country);
+      return feature ? projection(d3.geoCentroid(feature))[0] : null;
+    })
+    .attr("cy", d => {
+      const country = d.Country; 
+      const feature = geojson.features.find(f => f.properties.name === country);
+      return feature ? projection(d3.geoCentroid(feature))[1] : null;
+    });
+
+       // .attr("cx", 20)
+       // .attr("cy", 20)
+       // .attr("transform", d => {
+       //   const point = projection([d.longitude, d.latitude])
+       //  return `translate(${point[0]},${point[1]})`
+       //   })
 
 });
+
