07c44ed1d481c7a178fcb69a62510d13928c2d0f
thomagin <tom.fagin@gmail.com>
2024-04-29 14:28:11 -0400

    modified:   MoMA project/index.html 	modified:   MoMA project/main.js 	modified:   MoMA project/style.css

--- MoMA project/main.js
+++ MoMA project/main.js
@@ -1,5 +1,4 @@
 // GLOBALS AND CONSTANTS
-
 const width = 800;
 const height = 400;
 const margin = { top: 20, right: 20, bottom: 30, left: 50 };
@@ -7,7 +6,8 @@ const margin = { top: 20, right: 20, bottom: 30, left: 50 };
 // LOAD DATA CREATE SVG
 d3.csv('https://media.githubusercontent.com/media/thomagin/collection/main/Artworks.csv')
   .then(data => {
-    let svgChart = null; 
+    console.log(data);
+    let svgChart = null;
 
     // SELECT NATIONALITY 
     function updateChart(selectedNationality) {
@@ -20,7 +20,7 @@ d3.csv('https://media.githubusercontent.com/media/thomagin/collection/main/Artwo
       const sortedData = acquisitionsData.sort((a, b) => a.year - b.year);
 
       const xScale = d3.scaleBand()
-        .domain(sortedData.map(d => d.year.toString())) 
+        .domain(sortedData.map(d => d.year.toString()))
         .range([margin.left, width - margin.right])
         .padding(0.1);
 
@@ -49,9 +49,9 @@ d3.csv('https://media.githubusercontent.com/media/thomagin/collection/main/Artwo
         .attr('class', 'bar')
         .attr('x', d => xScale(d.year.toString()))
         .attr('width', xScale.bandwidth())
-        .attr('y', d => height - margin.bottom) 
-        .attr('height', 0) 
-        .attr('fill', 'steelblue')
+        .attr('y', d => height - margin.bottom)
+        .attr('height', 0)
+        .attr('fill', '#A98F88')
         .merge(bars)
         .transition()
         .duration(1000) // ANIMATION TIME, MSEC
@@ -63,14 +63,13 @@ d3.csv('https://media.githubusercontent.com/media/thomagin/collection/main/Artwo
         .attr('x', d => xScale(d.year.toString()))
         .attr('width', xScale.bandwidth());
 
-      svgChart.selectAll('g.axis').remove(); 
+      svgChart.selectAll('g.axis').remove();
 
       svgChart.append('g')
         .attr('class', 'axis')
         .attr('transform', `translate(0,${height - margin.bottom})`)
         .call(d3.axisBottom(xScale)
-          .tickFormat(d3.format('d')) 
-        )
+          .tickFormat(d3.format('d')))
         .selectAll('text')
         .style('text-anchor', 'end')
         .attr('transform', 'rotate(-45)');
@@ -80,22 +79,51 @@ d3.csv('https://media.githubusercontent.com/media/thomagin/collection/main/Artwo
         .attr('transform', `translate(${margin.left},0)`)
         .call(d3.axisLeft(yScale));
 
-      svgChart.selectAll('text.chart-title').remove(); 
+      svgChart.selectAll('text.chart-title').remove();
 
       svgChart.append('text')
         .attr('class', 'chart-title')
         .attr('x', width / 2)
-        .attr('y', margin.top / 2)
+        .attr('y', margin.top / 2 + 2)
         .attr('text-anchor', 'middle')
         .style('font-size', '16px')
         .text(`${selectedNationality} Art Acquisitions Over Time`);
+
+      // LITTLE PARAGRAPHS 
+      
+      const nationalityText = document.getElementById('nationality-text');
+      switch (selectedNationality) {
+        case '(Cuban)':
+          nationalityText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet leo ut tempus scelerisque. Pellentesque at nulla et erat molestie vehicula sit amet ut elit. Ut blandit enim nulla, et scelerisque turpis semper sed. Vivamus ex nulla, varius nec blandit at, sagittis et sem. Donec a urna ut velit tempus porta. Maecenas ullamcorper diam tempus tellus scelerisque vestibulum. Proin tristique lobortis egestas. Proin porta, turpis nec ultrices luctus, eros orci imperdiet erat, vel dapibus neque felis id ligula. Sed dignissim neque sed dignissim tempus. Mauris ornare ipsum id scelerisque porttitor. Nunc volutpat ex nulla, eget sodales eros sodales sed. Pellentesque congue suscipit arcu, non ultricies nulla. Maecenas viverra placerat hendrerit. Vivamus ligula lorem, faucibus ac tempus non, tristique ultrices magna. Donec non varius ligula.";
+          break;
+        case '(Iranian)':
+          nationalityText.textContent = "Vivamus suscipit eros sem, at laoreet erat scelerisque sit amet. Ut et vulputate diam. Quisque ut enim ac turpis ultricies scelerisque. Maecenas nec felis vel odio suscipit feugiat sit amet quis lacus. Fusce tristique diam eu purus venenatis, id tincidunt nunc interdum. Aenean eget nibh rutrum, sagittis nisi vitae, fringilla ligula. Suspendisse potenti.";
+          break;
+        case '(Chinese)':
+          nationalityText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet leo ut tempus scelerisque. Pellentesque at nulla et erat molestie vehicula sit amet ut elit. Ut blandit enim nulla, et scelerisque turpis semper sed. Vivamus ex nulla, varius nec blandit at, sagittis et sem. Donec a urna ut velit tempus porta. Maecenas ullamcorper diam tempus tellus scelerisque vestibulum. Proin tristique lobortis egestas. Proin porta, turpis nec ultrices luctus, eros orci imperdiet erat, vel dapibus neque felis id ligula. Sed dignissim neque sed dignissim tempus. Mauris ornare ipsum id scelerisque porttitor. Nunc volutpat ex nulla, eget sodales eros sodales sed. Pellentesque congue suscipit arcu, non ultricies nulla. Maecenas viverra placerat hendrerit. Vivamus ligula lorem, faucibus ac tempus non, tristique ultrices magna. Donec non varius ligula.";
+          break;
+        case '(German)':
+          nationalityText.textContent = "Vivamus suscipit eros sem, at laoreet erat scelerisque sit amet. Ut et vulputate diam. Quisque ut enim ac turpis ultricies scelerisque. Maecenas nec felis vel odio suscipit feugiat sit amet quis lacus. Fusce tristique diam eu purus venenatis, id tincidunt nunc interdum. Aenean eget nibh rutrum, sagittis nisi vitae, fringilla ligula. Suspendisse potenti.";
+          break;
+        case '(Japanese)':
+          nationalityText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet leo ut tempus scelerisque. Pellentesque at nulla et erat molestie vehicula sit amet ut elit. Ut blandit enim nulla, et scelerisque turpis semper sed. Vivamus ex nulla, varius nec blandit at, sagittis et sem. Donec a urna ut velit tempus porta. Maecenas ullamcorper diam tempus tellus scelerisque vestibulum. Proin tristique lobortis egestas. Proin porta, turpis nec ultrices luctus, eros orci imperdiet erat, vel dapibus neque felis id ligula. Sed dignissim neque sed dignissim tempus. Mauris ornare ipsum id scelerisque porttitor. Nunc volutpat ex nulla, eget sodales eros sodales sed. Pellentesque congue suscipit arcu, non ultricies nulla. Maecenas viverra placerat hendrerit. Vivamus ligula lorem, faucibus ac tempus non, tristique ultrices magna. Donec non varius ligula.";
+          break;
+        case '(Russian)':
+          nationalityText.textContent = "Vivamus suscipit eros sem, at laoreet erat scelerisque sit amet. Ut et vulputate diam. Quisque ut enim ac turpis ultricies scelerisque. Maecenas nec felis vel odio suscipit feugiat sit amet quis lacus. Fusce tristique diam eu purus venenatis, id tincidunt nunc interdum. Aenean eget nibh rutrum, sagittis nisi vitae, fringilla ligula. Suspendisse potenti.";
+          break;
+        case '(Ukrainian)':
+          nationalityText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet leo ut tempus scelerisque. Pellentesque at nulla et erat molestie vehicula sit amet ut elit. Ut blandit enim nulla, et scelerisque turpis semper sed. Vivamus ex nulla, varius nec blandit at, sagittis et sem. Donec a urna ut velit tempus porta. Maecenas ullamcorper diam tempus tellus scelerisque vestibulum. Proin tristique lobortis egestas. Proin porta, turpis nec ultrices luctus, eros orci imperdiet erat, vel dapibus neque felis id ligula. Sed dignissim neque sed dignissim tempus. Mauris ornare ipsum id scelerisque porttitor. Nunc volutpat ex nulla, eget sodales eros sodales sed. Pellentesque congue suscipit arcu, non ultricies nulla. Maecenas viverra placerat hendrerit. Vivamus ligula lorem, faucibus ac tempus non, tristique ultrices magna. Donec non varius ligula.";
+          break;
+        default:
+          nationalityText.textContent = "Select a nationality from the dropdown to see related information.";
+      }
     }
 
     // LANDING CHART
     updateChart('(Cuban)');
 
     // EVENT LISTENER FOR DROPDOWN
-    document.getElementById('nationality-select').addEventListener('change', function() {
+    document.getElementById('nationality-select').addEventListener('change', function () {
       const selectedNationality = this.value;
       updateChart(selectedNationality);
     });
@@ -103,4 +131,3 @@ d3.csv('https://media.githubusercontent.com/media/thomagin/collection/main/Artwo
   .catch(error => {
     console.error('Error loading data:', error);
   });
-
